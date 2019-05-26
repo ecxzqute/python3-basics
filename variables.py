f25dd3befa66ad045b8ef7a6a1583110b3ef598a
Ja Bune <jabune888@gmail.com>
2019-05-26 23:01:33 +0800

    done with python variables

--- variables.py
+++ variables.py
@@ -13,3 +13,36 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+# INDIVIDUAL VARIABLE ASSIGNMENT
+x = 1           # int
+y = 2.9         # float
+name = "John"   # str    String
+isTrue = True   # bool  Boolean
+
+# MULTIPLE ASSIGNMENTS
+x2, y2, name2, isTrue2 = (100, 2.345, "Kaycee", True)
+
+
+# BASIC MATH
+x3 = x + x2
+y3 = y + y2
+
+#print(x3, y3)
+# OUTPUT >> 101 5.245
+
+# CHECK VARIABLE TYPE
+#print(type(x), type(y), type(name), type(isTrue))
+# OUTPUT >> <class 'int'> <class 'float'> <class 'str'> <class 'bool'>
+
+# TYPE CASTING
+#x = str(x)
+# print(type(x))
+# OUTPUT >> <class 'str'>
+
+# OUTPUT
+# BASIC
+print('hello')
+
+# MULTIPLE
+print(x, y, name, isTrue)
